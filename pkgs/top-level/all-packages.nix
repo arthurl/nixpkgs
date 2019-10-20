e1091528dce51aaf5769028e599a86f4eceae2cf
Arthur Lee <me@arthur.li>
2019-10-20 10:56:28 +0800

    lms: Init at v3.27.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19201,6 +19201,8 @@ in
 
   livepeer = callPackage ../servers/livepeer { };
 
+  lms = callPackage ../servers/misc/lms { };
+
   lwan = callPackage ../servers/http/lwan { };
 
   labelImg = callPackage ../applications/science/machine-learning/labelimg { };
